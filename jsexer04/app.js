c7f795515ffc82c2973da1ed14ef043cfc30b255
Gem <gemralph@yahoo.com>
2019-04-08 12:15:05 +0900

    week 6 jsexer04

--- jsexer04/app.js
+++ jsexer04/app.js
@@ -20,22 +20,34 @@ function jojang(){
     //console.log(fn);
     //console.log(phone);
 
-    tb = document.querySelector("tbody");
+    tb = document.querySelector("#datax");
     mtr = document.createElement("tr");
-    /*mtr.innerHTML = "<td>"+ ln +"</td><td>" 
+    /*
+    //short but difficult when too many data;
+    mtr.innerHTML = "<td>"+ ln +"</td><td>" 
                             +fn + "</td><td>" 
                             + phone + "</td>";*/
+   
+   //this method is easy to understand
     td1 = document.createElement("td");
     td2 = document.createElement("td");
     td3 = document.createElement("td");
+    td4 = document.createElement("td");
     td1.innerText = ln;
     td2.innerText = fn;
     td3.innerText = phone;
+    td4.classList = "text-center";
+    td4.innerHTML = "<img onclick='sakje(this);' src='images/delete.svg'/>";
 
     mtr.append(td1);
     mtr.append(td2);
     mtr.append(td3);
+    mtr.append(td4);
     tb.append(mtr);
+}
 
+function sakje(obj){
+    //console.log(obj);
+    obj.parentNode.parentNode.remove();
 
 }
